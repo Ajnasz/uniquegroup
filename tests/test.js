595537b9a039814836b828789dee44b56f98c07b
Lajos Koszti <ajnasz@ajnasz.hu>
2011-11-19 23:10:23 +0100

    Extend tests
    Test FileGroup and UniqueGroup separately

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,46 @@
 /*global require: true, exports: true*/
-var assert = require('assert');
-var FileGroup = require('../index').FileGroup;
+const assert = require('assert');
+const uniquegroup = require('../index');
+const FileGroup = uniquegroup.FileGroup;
+const UniqueGroup = uniquegroup.UniqueGroup;
 
 
-var common = new FileGroup([
+var common = new UniqueGroup([
     'files/aaa.js',
     'files/bbb.js',
     'files/ccc.js'
 ]);
-var foobar1 = new FileGroup([
+var foobar1 = new UniqueGroup([
     'files/ddd.js',
     'files/eee.js',
     'files/fff.js'
 ]);
-var foobar2 = new FileGroup([
+var foobar2 = new UniqueGroup([
     'files/ccc.js',
     common,
     'files/111.js',
     foobar1
 ]);
+var foobar4 = new FileGroup([
+    'files/ccc.js',
+]);
+var foobar3 = new FileGroup([
+    foobar4,
+    common,
+    'files/111.js',
+    foobar1
+]);
 
-foobar2.generate(function (data) {
+(function testUniqueGroup() {
+    var files = ['files/ccc.js', 'files/aaa.js', 'files/bbb.js', 'files/111.js', 'files/ddd.js', 'files/eee.js', 'files/fff.js'], i;
+    assert.deepEqual(foobar2.items.length, files.length);
+    for (i = 0;i < files.length; i += 1) {
+        assert.deepEqual(foobar2.items[i], files[i]);
+    }
+}());
+
+
+foobar3.generate(function (data) {
     var dataArr = data.split('\n'),
         items = ['ccc', 'aaa', 'bbb', '111', 'ddd', 'eee', 'fff', ''],
         i;
